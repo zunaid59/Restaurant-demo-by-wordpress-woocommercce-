65297123f50b7e964db2ec872c98822f428a5251
Abunl Kashem Md. Zunaid <zunaidonlineincome@gmail.com>
2019-07-08 07:40:15 +0600

    site build as properly

---
+++ remove-code.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ remove code from home page
+ */
+//
+//[vc_column][/vc_column][/vc_row][vc_row el_class="td-ss-row"]
+//[vc_column width="2/3"][td_block_16 custom_title="Latest Blog Posts" limit="6" ajax_pagination="next_prev" sort="random_posts"]
+//[/vc_column][vc_column width="1/3"][td_block_7 custom_title="Most Popular" sort="random_posts" limit="3" ajax_pagination="next_prev"]
+//[/vc_column][/vc_row]
